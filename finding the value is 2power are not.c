b75ab90bce632b0e599c44a6738bcf14c0a45bf1
naveen-06-12-1999 <98304137+naveen-06-12-1999@users.noreply.github.com>
2022-02-23 22:33:29 +0530

    finding the value is 2power are not

---
+++ finding the value is 2power are not.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=6;
+    if((n&n-1)==0)
+    printf("the number is 2power n");
+    else
+    printf("the number is not 2power n");
+    
+
+    return 0;
+}
